72904918a7161b23d50f6b3505fb6bd8fc6028f2
alishba <itsalishba00@gmail.com>
2025-05-11 09:16:01 +0530

    add files

---
+++ GameFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    public GameFrame() {
+        this.setTitle("Floppy Fish");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.add(new GamePanel());
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
